4f531464b22c68462e216639e25528999100fb2f
darshnablah <dwright758@gmail.com>
2020-07-18 16:42:38 -0500

    created markdown

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,45 @@
-// function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+const generateContributing = contributingText => {
+  if (!contributingText) {
+      return '';
+  }
+  return `
+      ${contributingText}
+  `
+}
 
+// function to generate markdown for README
+const generateMarkdown = data => {
+  return `
+  #${data.title}
+  
+  ##Description
+  ${data.description}
+
+  ##Table of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+  
+  ##Installation
+  ${data.installation}
+
+  ##Usage
+  ${data.usage}
+
+  ##License
+  ${data.license}
+
+  ##Contributing
+  ${generateContributing(contributing)}
+
+  ##Tests
+  ${data.tests}
+
+  ##Questions
+  For questions, please contact me at: **github.com/${data.github}**, or via email at: **${data.email}**.
 `;
 }
 
